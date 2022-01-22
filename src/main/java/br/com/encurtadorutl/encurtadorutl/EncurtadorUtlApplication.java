ae0418f96487db868f9ce331f44445e1be10e806
RodResq <rodrigoresque@yahoo.com.br>
2022-01-22 11:12:40 -0300

    initial commit

---
+++ src/main/java/br/com/encurtadorutl/encurtadorutl/EncurtadorUtlApplication.java
@@ -0,0 +1,13 @@
+package br.com.encurtadorutl.encurtadorutl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncurtadorUtlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncurtadorUtlApplication.class, args);
+	}
+
+}
