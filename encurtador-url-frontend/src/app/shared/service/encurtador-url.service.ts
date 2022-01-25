bdb25b3069e0a3fab4bed330c4b7817e9ae10f22
RodResq <rodrigoresque@yahoo.com.br>
2022-01-24 21:53:35 -0300

    add front end folders

---
+++ encurtador-url-frontend/src/app/shared/service/encurtador-url.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {environment} from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EncurtadorUrlService {
+
+  private readonly apiEncurtadorUrl = `${environment.URL_BACKEND}/test`;
+
+  constructor(private http: HttpClient) { }
+
+  encurtadorTest(): Observable<any> {
+    const httpOptions = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/text' })
+    }
+    return this.http.get(`${this.apiEncurtadorUrl}`, httpOptions);
+  }
+}
