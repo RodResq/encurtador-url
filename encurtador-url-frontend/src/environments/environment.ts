8318f7f4bdd44ef5a142a347d9a42f6d294d98e2
RodResq <rodrigoresque@yahoo.com.br>
2022-02-02 19:02:09 -0300

    updt environemt

--- encurtador-url-frontend/src/environments/environment.ts
+++ encurtador-url-frontend/src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   contextPath: '/',
   url_base: 'http://localhost:8080',
   ambient: 'dev'
